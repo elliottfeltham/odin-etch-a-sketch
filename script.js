29d2af6fd33715142bb7ac7bb2e615ab77791f1d
elliottfeltham <elliottfeltham98@gmail.com>
2025-01-22 22:31:40 +0000

    Add working paint function on mousedown

---
+++ script.js
@@ -0,0 +1,40 @@
+const container = document.querySelector(".container");
+
+function createGrid(axis) {
+    let gridSize = axis * axis
+    for (let i = 0; i < gridSize; i++) {
+        const gridBox = document.createElement("div");
+        gridBox.classList.add("gridBox");
+        container.append(gridBox);
+    }
+}
+
+// Function to paint only when mouse is clicked down
+function paint () {
+
+    const allGridBoxes = document.querySelectorAll(".gridBox");
+
+    let isMouseDown = false; 
+
+
+    document.addEventListener("mousedown", function() {
+        isMouseDown = true;  
+    });
+
+
+    document.addEventListener("mouseup", function() {
+        isMouseDown = false; 
+    });
+        
+        
+    allGridBoxes.forEach((gridBox) => {
+        gridBox.addEventListener("mouseover", function () {
+            if (isMouseDown == true) {
+            gridBox.style.backgroundColor = "black";
+        }});
+    });
+}
+
+createGrid(16);
+
+paint();
