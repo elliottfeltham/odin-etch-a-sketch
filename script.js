654c6546147bc4e8c7aa2dfcd0f4849ba6ffd03c
elliottfeltham <elliottfeltham98@gmail.com>
2025-01-23 18:53:27 +0000

    Finish basic etch-a-sketch

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 const container = document.querySelector(".container");
+const gridSizeButton = document.querySelector("#grid-size")
+
+// Function to prompt the user for a grid size
+function getGridSize() {
+    let gridSize
+    do {
+       gridSize = prompt("Select a grid size between 0 and 100!");
+
+    
+       if (gridSize === null) {
+        return null;
+       }
+    } while (gridSize < 1 || gridSize > 100 || isNaN(gridSize));
+
+    return gridSize
+}
 
 // Function to create the grid
 function createGrid(size) {
-    let gridSize = size * size
-    for (let i = 0; i < gridSize; i++) {
+    let totalSize = size * size
+    for (let i = 0; i < totalSize; i++) {
         const gridBox = document.createElement("div");
         gridBox.classList.add("gridBox");
+        gridBox.style.height = `${100 / size}%`;
+        gridBox.style.width = `${100 / size}%`;
         container.append(gridBox);
     }
+
+    paint();
 }
 
 // Function to paint only when mouse is clicked down
 function paint () {
 
     const allGridBoxes = document.querySelectorAll(".gridBox");
+    
 
     let isMouseDown = false; 
 
@@ -36,6 +57,19 @@ function paint () {
     });
 }
 
-createGrid(16);
+// Function to reset canvas to white  
+function resetGrid() {
+    const allGridBoxes = document.querySelectorAll(".gridBox");
+    
+    allGridBoxes.forEach((gridBox) => {
+            gridBox.style.backgroundColor = "white";
+})};
+    
+ 
 
-paint();
+gridSizeButton.addEventListener("click", () => {
+    container.innerHTML = "";
+    let gridSize = getGridSize();
+    createGrid(gridSize);
+});
+
